82bcd4cbf5a9fbfebacf0fe588d496c8e3a8da3e
Suraj977327 <surajsingh977327@gmail.com>
2026-06-26 10:00:05 +0530

    solved

--- index.js
+++ index.js
@@ -40,6 +40,145 @@
 
 // --------------------------------------------------------------------------------------------------------------
 
+// Array Leaders
+// let arr = [16, 17, 4, 3, 5, 2];
+// let result = [];
+// for (let i = 0; i < arr.length; i++) {
+//   let count = true;
+//   for (let j = i + 1; j < arr.length; j++) {
+//     if (arr[j] > arr[i]) {
+//       count = false;
+//       break;
+//     }
+//   }
+//   if (count) {
+//     result.push(arr[i]);
+//   }
+// }
+// console.log(result);
+
+// --------------------------------------------------------------------------------------------------------------
+
+// Duplicates in a Limited Range Array
+// let arr = [2, 3, 1, 2, 3];
+// let result = [];
+// let dup = [];
+// for (let i = 0; i < arr.length; i++) {
+//   if (result.includes(arr[i])) {
+//     dup.push(arr[i]);
+//   } else {
+//     result.push(arr[i]);
+//   }
+// }
+// console.log(dup);
+
+// --------------------------------------------------------------------------------------------------------------
+
+// First Occurrence in Sorted
+// let arr = [11, 22, 33, 44, 55];
+// function Appears(num) {
+//   for (let i = 0; i < arr.length; i++) {
+//     if (arr[i] === num) {
+//       return i;
+//       break;
+//     }
+//   }
+//   return -1;
+// }
+// console.log(Appears(22));
+
+// --------------------------------------------------------------------------------------------------------------
+
+// Equilibrium Point
+// function equilibriumIndex(arr) {
+//   for (let i = 0; i < arr.length; i++) {
+//     let leftSum = 0;
+//     let rightSum = 0;
+//     for (let j = 0; j < i; j++) {
+//       leftSum += arr[j];
+//     }
+//     for (let j = i + 1; j < arr.length; j++) {
+//       rightSum += arr[j];
+//     }
+//     if (leftSum === rightSum) {
+//       return i;
+//     }
+//   }
+//   return -1;
+// }
+// console.log(equilibriumIndex([-7, 1, 5, 2, -4, 3, 0]));
+
+// --------------------------------------------------------------------------------------------------------------
+
+// Two Sum - Pair with Given Sum
+// let arr = [11];
+// function Addition(num) {
+//   for (let i = 0; i < arr.length; i++) {
+//     if (arr[i] + arr[i + 1] == num) {
+//       return true;
+//       console.log(arr[i], arr[i + 1]);
+//     }
+//   }
+//   return false;
+// }
+// console.log(Addition(11));
+
+// --------------------------------------------------------------------------------------------------------------
+
+// Two sum -Pairs with 0 Sum
+// let arr = [6, 1, 8, 0, 4, -9, -1, -10, -6, -5];
+// let result = [];
+// for (let i = 0; i < arr.length; i++) {
+//   for (let j = i + 1; j < arr.length; j++) {
+//     if (arr[i] + arr[j] == 0) {
+//       result.push([arr[i], arr[j]]);
+//     }
+//   }
+// }
+// console.log(result);
+
+// --------------------------------------------------------------------------------------------------------------
+
+// Union of Arrays with Duplicates
+// let a = [1, 2, 3, 2, 1];
+// let b = [3, 2, 2, 3, 3, 2];
+// let set = new Set(a, b);
+// console.log([...set]);
+
+// let a = [1, 2, 1, 1, 2];
+// let b = [2, 2, 1, 2, 1];
+// let c = [];
+// for (let i = 0; i < a.length; i++) {
+//   if (!c.includes(a[i])) {
+//     c.push(a[i]);
+//   }
+// }
+// for (let j = 0; j < b.length; j++) {
+//   if (!c.includes(b[j])) {
+//     c.push(b[j]);
+//   }
+// }
+// console.log(c);
+
+// --------------------------------------------------------------------------------------------------------------
+
+// Move All Zeroes to End
+// let arr = [1, 2, 0, 4, 3, 0, 5, 0];
+// let result = [];
+// for (let i = 0; i < arr.length; i++) {
+//   if (arr[i] !== 0) {
+//     result.push(arr[i]);
+//   }
+// }
+// for (let i = 0; i < arr.length; i++) {
+//   if (arr[i] == 0) {
+//     result.push(arr[i]);
+//   }
+// }
+// console.log(result);
+
+// --------------------------------------------------------------------------------------------------------------
+
 // Check Equal Arrays
 
 // let a = [1, 2, 5, 4, 0];
